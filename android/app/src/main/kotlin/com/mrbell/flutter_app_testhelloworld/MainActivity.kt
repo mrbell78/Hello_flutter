c50c9f9824d097650b8959e94cc81ba017b94960
Nurhossen <mrbell12131103078@gmail.com>
2019-09-15 01:18:01 +0600

    simple Hello world with flutter

---
+++ android/app/src/main/kotlin/com/mrbell/flutter_app_testhelloworld/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mrbell.flutter_app_testhelloworld
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+void main(){
+ 
+  
+  runApp(
+
+    Center(
+
+       child: Text("Hello flutter by rubel", textDirection: TextDirection.ltr,)
+
+    )
+
+  );
+}
